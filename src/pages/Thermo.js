18dc612c05fd2b98532a3225446c95a0f8c94349
Dmitrii <Dokter322@yandex.ru>
2022-12-22 20:16:48 +0300

    added sos btn

--- src/pages/Thermo.js
+++ src/pages/Thermo.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Modal from "../components/Modal";
 import MyInput from "../components/MyInput";
 import QuestionThermo from "../components/QuestionThermo";
 import { dataThermo } from "../dataThermo";
@@ -6,6 +7,7 @@ import "../styles/Thermo.css";
 const About = () => {
   const [data, setData] = useState(dataThermo);
   const [value, setValue] = useState("");
+  const [modalActive, setModalActive] = useState(false);
 
   useEffect(() => {
     const newData = dataThermo.filter((obj) =>
@@ -14,15 +16,23 @@ const About = () => {
     setData(newData);
   }, [value]);
   return (
-    <>
+    <div className="holder">
       <h1 className="title">Термодинамика</h1>
+      <Modal active={modalActive} setActive={setModalActive}>
+        Кнопка на случай поноса
+      </Modal>
+
+      <button className="btn-sos" onClick={() => setModalActive(true)}>
+        SOS
+      </button>
       <MyInput value={value} setValue={setValue} placeholder={"по номеру"} />
+
       <main>
         {data.map((obj) => {
           return <QuestionThermo obj={obj} key={obj.id} />;
         })}
       </main>
-    </>
+    </div>
   );
 };
 
