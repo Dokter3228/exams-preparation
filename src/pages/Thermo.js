e6809e9d68ab2333f0a3cafb989b276a5cdf1610
Dmitrii <Dokter322@yandex.ru>
2022-12-23 14:58:18 +0300

    changes

--- src/pages/Thermo.js
+++ src/pages/Thermo.js
@@ -2,19 +2,18 @@ import React, { useState, useEffect } from "react";
 import Modal from "../components/Modal";
 import MyInput from "../components/MyInput";
 import QuestionThermo from "../components/QuestionThermo";
-import { dataThermo } from "../dataThermo";
 import "../styles/Thermo.css";
 const About = () => {
-  const [data, setData] = useState(dataThermo);
-  const [value, setValue] = useState("");
+  // const [data, setData] = useState(dataThermo);
+  // const [value, setValue] = useState("");
   const [modalActive, setModalActive] = useState(false);
 
-  useEffect(() => {
-    const newData = dataThermo.filter((obj) =>
-      value != "" ? obj.id == value : obj.id
-    );
-    setData(newData);
-  }, [value]);
+  // useEffect(() => {
+  //   const newData = dataThermo.filter((obj) =>
+  //     value != "" ? obj.id == value : obj.id
+  //   );
+  //   setData(newData);
+  // }, [value]);
   return (
     <div className="holder">
       <h1 className="title">Термодинамика</h1>
@@ -123,19 +122,98 @@ const About = () => {
           для нагревания единицы массы вещества на 1 К: (Дж/кг·К).
         </p>
       </Modal>
-
       <button
         className={modalActive ? "btn-sos active" : "btn-sos"}
         onClick={() => setModalActive(true)}
       >
         SOS
       </button>
-      <MyInput value={value} setValue={setValue} placeholder={"по номеру"} />
+      {/* ОСНОВА */}
+      {/* <MyInput value={value} setValue={setValue} placeholder={"по номеру"} />
 
       <main>
         {data.map((obj) => {
           return <QuestionThermo obj={obj} key={obj.id} />;
         })}
+      </main> */}
+      <main>
+        <div className="question-container">
+          <h2 className="question">dfasdfas</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+            totam voluptas quisquam error ut soluta similique pariatur. Eaque,
+            sit nisi laboriosam est esse asperiores aperiam, architecto amet
+            eius ea at sint quam. Consequuntur est magni alias impedit? Odio
+            tenetur sint iure laborum beatae pariatur iste, perspiciatis velit
+            officia libero mollitia.
+          </p>
+        </div>
+        <div className="question-container">
+          <h2 className="question">dfasdfas</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+            totam voluptas quisquam error ut soluta similique pariatur. Eaque,
+            sit nisi laboriosam est esse asperiores aperiam, architecto amet
+            eius ea at sint quam. Consequuntur est magni alias impedit? Odio
+            tenetur sint iure laborum beatae pariatur iste, perspiciatis velit
+            officia libero mollitia.
+          </p>
+        </div>
+        <div className="question-container">
+          <h2 className="question">dfasdfas</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+            totam voluptas quisquam error ut soluta similique pariatur. Eaque,
+            sit nisi laboriosam est esse asperiores aperiam, architecto amet
+            eius ea at sint quam. Consequuntur est magni alias impedit? Odio
+            tenetur sint iure laborum beatae pariatur iste, perspiciatis velit
+            officia libero mollitia.
+          </p>
+        </div>
+        <div className="question-container">
+          <h2 className="question">dfasdfas</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+            totam voluptas quisquam error ut soluta similique pariatur. Eaque,
+            sit nisi laboriosam est esse asperiores aperiam, architecto amet
+            eius ea at sint quam. Consequuntur est magni alias impedit? Odio
+            tenetur sint iure laborum beatae pariatur iste, perspiciatis velit
+            officia libero mollitia.
+          </p>
+        </div>
+        <div className="question-container">
+          <h2 className="question">dfasdfas</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+            totam voluptas quisquam error ut soluta similique pariatur. Eaque,
+            sit nisi laboriosam est esse asperiores aperiam, architecto amet
+            eius ea at sint quam. Consequuntur est magni alias impedit? Odio
+            tenetur sint iure laborum beatae pariatur iste, perspiciatis velit
+            officia libero mollitia.
+          </p>
+        </div>
+        <div className="question-container">
+          <h2 className="question">dfasdfas</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+            totam voluptas quisquam error ut soluta similique pariatur. Eaque,
+            sit nisi laboriosam est esse asperiores aperiam, architecto amet
+            eius ea at sint quam. Consequuntur est magni alias impedit? Odio
+            tenetur sint iure laborum beatae pariatur iste, perspiciatis velit
+            officia libero mollitia.
+          </p>
+        </div>
+        <div className="question-container">
+          <h2 className="question">dfasdfas</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+            totam voluptas quisquam error ut soluta similique pariatur. Eaque,
+            sit nisi laboriosam est esse asperiores aperiam, architecto amet
+            eius ea at sint quam. Consequuntur est magni alias impedit? Odio
+            tenetur sint iure laborum beatae pariatur iste, perspiciatis velit
+            officia libero mollitia.
+          </p>
+        </div>
       </main>
     </div>
   );
